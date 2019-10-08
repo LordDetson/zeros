4c400259453a915391f8b1a442825fe93d114014
Lord_Detson <babanin.dima@gmail.com>
2019-10-08 22:09:23 +0300

    feat: implemented function zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function zeros(expression) {
-  // your solution
+    let array = expression.split("*");
+    let result = 1n;
+    for (let i = 0; i < array.length; i++) {
+        let buff = BigInt(parseInt(array[i]));
+        let h;
+        if (array[i].match("!!") == null) {
+            h = 1n;
+        } else {
+            h = 2n;
+        }
+        for (; buff > 1n ; buff-=h) {
+            result *= buff;
+        }
+    }
+    let strResult = result.toString();
+    let count = 0;
+    while (strResult[strResult.length - count - 1] === "0") {
+        count++;
+    }
+    return count;
 }
